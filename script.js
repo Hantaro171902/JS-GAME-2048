5bd318ab6eb4b5783ddc9d1fac863d32ee292948
Hantaro191702 <chaugiahan001@gmail.com>
2024-11-14 18:49:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let grid = document.querySelector(".grid");
+const startButton = document.getElementById("start-button");
+const container = document.querySelector(".container");
+const coverScreen = document.querySelector("cover-screen");
