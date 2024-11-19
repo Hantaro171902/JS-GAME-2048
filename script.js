9ebf2313c085c1bb558c65b4902ca557dca9e170
Hantaro191702 <chaugiahan001@gmail.com>
2024-11-19 14:20:28 +0700

    update

--- script.js
+++ script.js
@@ -86,9 +86,11 @@ const gameoverCheck = () => {
 
 const generateTwo = () => {
   if (hasEmptyBox()) {
+    // Check if there are any empty box (value=0)
     let randomRow = randomPosition(matrix);
     let randomCol = randomPosition(matrix[randomPosition(matrix)]);
     if (matrix[randomRow][randomCol] == 0) {
+      // checkk if position is empty
       matrix[randomRow][randomCol] = 2;
       let element = document.querySelector(
         `[data-position = ${randomRow}_${randomCol}]`
@@ -100,5 +102,65 @@ const generateTwo = () => {
     }
   } else {
     gameoverCheck();
+  }
+};
+
+const generateFour = () => {
+  if (hasEmptyBox()) {
+    let randomRow = randomPosition(matrix);
+    let randomCol = randomPosition(matrix[randomPosition(matrix)]);
+    if (matrix[randomRow][randomCol] == 0) {
+      matrix[randomRow][randomCol] = 4;
+      let element = document.querySelector(
+        `[data-position= ${randomCol}_${randomCol}]`
+      );
+      element.innerHTML = 4;
+      element.classList.add("box-4");
+    } else {
+      generateFour();
+    }
+  } else {
+    gameoverCheck();
+  }
+};
+
+const removeZero = (arr) => arr.filter((num) => num);
+const checker = (arr, reverseArr = false) => {
+  arr = reverseArr ? remove(arr).reverse() : removeZero(arr);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] == arr[i + 1]) {
+      arr[i] += arr[i + 1];
+      arr[i + 1] = 0;
+      score += arr[i];
+    }
+  }
+
+  arr = reverseArr ? remove(arr).reverse() : removeZero(arr);
+
+  let missingCount = 4 - arr.length;
+  while (missingCount > 0) {
+    if (reverseArr) {
+      arr.unshilf(0);
+    } else {
+      arr.push(0);
+    }
+    missingCount -= 1;
+  }
+  return arr;
+};
+
+const sildeDown = () => {
+  for (let i = 0; i < columns; i++) {
+    let num = [];
+    for (let j = 0; j < rows; j++) {
+      num.push(matrix[j][i]);
+    }
+    num = checker(num, true);
+    for (let j = 0; j < rows; j++) {
+      matrix[j][i] = num[j];
+      let element = document.querySelector(`
+        [data-position=${j}_${i}]`);
+    }
   }
 };
